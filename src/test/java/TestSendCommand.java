82796c6cf3b9b913a41f799615274a2a060fea97
Peter <peter@quantr.hk>
2025-06-07 19:20:16 +0800

    dev

--- src/test/java/TestSendCommand.java
+++ src/test/java/TestSendCommand.java
@@ -15,14 +15,12 @@ public class TestSendCommand {
 								"\n" + //
 								"# Get x and y from script arguments, default to 0 if not provided\n" + //
 								"print(sys.argv)\n" + //
-								"x = int(sys.argv[1]) if len(sys.argv) > 1 else 10\n" + //
-								"y = int(sys.argv[2]) if len(sys.argv) > 2 else 30\n" + //
 								"\n" + //
 								"i2c = I2C(1)  # Create I2C object\n" + //
 								"display = ssd1306.SSD1306_I2C(128, 64, i2c)  # Pass I2C object\n" + //
 								"\n" + //
 								"display.fill(0)\n" + //
-								"display.text(\"Hello, World\", x, y)\n" + //
+								"display.text(\"Hello, World\", 10, 20)\n" + //
 								"display.show()\n" + //
 								"\n";
 			String result = JavaMPremote.sendCommand(command);
